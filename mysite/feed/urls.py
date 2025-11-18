17e57f9fe39c6fedc3525da27676e5d729a47ac7
Faisal Abraham <neicgecko@gmail.com>
2025-11-18 22:20:26 +0700

    482

--- mysite/feed/urls.py
+++ mysite/feed/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, PostDetailView
 
 app_name = 'feed'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('detail/<int:pk>/<str:name>/', PostDetailView.as_view(), name='detail'),
 ]
